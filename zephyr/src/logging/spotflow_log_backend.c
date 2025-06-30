ac210cdf19adb3aaa333da5450d01284d9ee4bb4
jmasek <jan.masek@datamole.cz>
2025-06-30 16:19:44 +0200

    added fix for v4.1.0

--- zephyr/src/logging/spotflow_log_backend.c
+++ zephyr/src/logging/spotflow_log_backend.c
@@ -152,7 +152,8 @@ static int drop_log_msg_from_queue(struct spotflow_log_context* ctx)
 		but it is unlikely because message_index was already increased when added to buffer,
 		only statistic, keeping it as is */
 		ctx->dropped_backend_count++;
-		LOG_DBG("Dropped oldest message");
+		/* currently not logged because it is messing up the output significantly */
+		/*LOG_DBG("Dropped oldest message");*/
 	}
 	return rc;
 }
