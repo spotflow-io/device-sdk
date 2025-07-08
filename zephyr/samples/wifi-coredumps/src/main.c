c763f6a5cb7581284e87289dfd0ddae73a33e67f
jmasek <jan.masek@datamole.cz>
2025-07-08 14:29:35 +0200

    added wifi-coredumps sample
    # Conflicts:
    #	zephyr/samples/wifi-logs/src/main.c

--- zephyr/samples/wifi-coredumps/src/main.c
+++ zephyr/samples/wifi-coredumps/src/main.c
@@ -1,4 +1,3 @@
-#include <zephyr/bindesc.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -7,11 +6,16 @@
 
 LOG_MODULE_REGISTER(MAIN, LOG_LEVEL_INF);
 
-/* Uncomment this function to provide your own device ID in runtime */
-/*const char* spotflow_override_device_id()
+void dump_coredump()
 {
-	return "my_nrf7002dk_test";
-}*/
+	/* … */
+	unsigned int reason = K_ERR_KERNEL_OOPS; /* or K_ERR_KERNEL_PANIC, etc. */
+	const struct arch_esf* esf = NULL; /* no exception context */
+	struct k_thread* thr = k_current_get(); /* dump the current thread */
+
+	coredump(reason, esf, thr);
+	/* Note: after this the dump will be emitted but your code keeps running */
+}
 
 int main(void)
 {
@@ -22,7 +26,8 @@ int main(void)
 
 	init_wifi();
 	connect_to_wifi();
-
+	LOG_INF("going to dump coredump");
+	dump_coredump();
 	for (int i = 0; i < 20; i++) {
 		LOG_INF("Hello from Zephyr to Spotflow: %d", i);
 		k_sleep(K_SECONDS(2));
