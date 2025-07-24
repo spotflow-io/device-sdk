10c8ea1d95019fd0751bf0253e3a859e9806f48d
roberthusak <robert@husak.cloud>
2025-07-24 15:08:14 +0200

    Remove obtaining build ID from sample code
    It's supposed to be used only within the module.

--- zephyr/samples/wifi/src/main.c
+++ zephyr/samples/wifi/src/main.c
@@ -2,8 +2,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#include "spotflow_build_id.h"
-
 #include "wifi.h"
 
 LOG_MODULE_REGISTER(MAIN, LOG_LEVEL_INF);
@@ -11,12 +9,6 @@ LOG_MODULE_REGISTER(MAIN, LOG_LEVEL_INF);
 int main(void)
 {
 	LOG_INF("Starting Spotflow logging example");
-
-	uint8_t* build_id;
-	uint16_t build_id_len;
-	spotflow_build_id_get(&build_id, &build_id_len);
-
-	LOG_HEXDUMP_INF(build_id, build_id_len, "Build ID:");
 
 	// Wait for the initialization of Wi-Fi device
 	k_sleep(K_SECONDS(1));
