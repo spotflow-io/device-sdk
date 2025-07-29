b809ebf51d48d46116aba496bc8f10b1f97c1872
roberthusak <robert@husak.cloud>
2025-07-29 11:37:09 +0200

    Investigate infinite loop in Python tests

--- zephyr/scripts/tests/test_patch_build_id.py
+++ zephyr/scripts/tests/test_patch_build_id.py
@@ -30,43 +30,43 @@ def test_patch_build_id():
         assert strip_filepath.exists()
         assert exe_filepath.exists()
 
-        patch_build_id.generate_and_patch_build_id(
-            str(elf_filepath),
-            [
-                str(hex_filepath),
-                str(bin_filepath),
-                str(strip_filepath),
-                str(exe_filepath),
-            ],
-        )
+        # patch_build_id.generate_and_patch_build_id(
+        #     str(elf_filepath),
+        #     [
+        #         str(hex_filepath),
+        #         str(bin_filepath),
+        #         str(strip_filepath),
+        #         str(exe_filepath),
+        #     ],
+        # )
 
-        elf_bytes = elf_filepath.read_bytes()
-        hex_bytes = hex_filepath.read_bytes()
-        bin_bytes = bin_filepath.read_bytes()
-        strip_bytes = strip_filepath.read_bytes()
-        exe_bytes = exe_filepath.read_bytes()
+        # elf_bytes = elf_filepath.read_bytes()
+        # hex_bytes = hex_filepath.read_bytes()
+        # bin_bytes = bin_filepath.read_bytes()
+        # strip_bytes = strip_filepath.read_bytes()
+        # exe_bytes = exe_filepath.read_bytes()
 
-        expected_elf_filepath = outputs_dir / "zephyr.elf"
-        expected_hex_filepath = outputs_dir / "zephyr.hex"
-        expected_bin_filepath = outputs_dir / "zephyr.bin"
-        expected_strip_filepath = outputs_dir / "zephyr.strip"
-        expected_exe_filepath = outputs_dir / "zephyr.exe"
+        # expected_elf_filepath = outputs_dir / "zephyr.elf"
+        # expected_hex_filepath = outputs_dir / "zephyr.hex"
+        # expected_bin_filepath = outputs_dir / "zephyr.bin"
+        # expected_strip_filepath = outputs_dir / "zephyr.strip"
+        # expected_exe_filepath = outputs_dir / "zephyr.exe"
 
-        expected_build_id = bytes.fromhex("f9931dd776ccc422a576d80eaed1c0fcca8f0331")
+        # expected_build_id = bytes.fromhex("f9931dd776ccc422a576d80eaed1c0fcca8f0331")
 
-        assert elf_bytes[0x260 : 0x260 + len(expected_build_id)] == expected_build_id
-        assert bin_bytes[0x160 : 0x160 + len(expected_build_id)] == expected_build_id
-        assert strip_bytes[0x260 : 0x260 + len(expected_build_id)] == expected_build_id
-        assert exe_bytes[0x260 : 0x260 + len(expected_build_id)] == expected_build_id
+        # assert elf_bytes[0x260 : 0x260 + len(expected_build_id)] == expected_build_id
+        # assert bin_bytes[0x160 : 0x160 + len(expected_build_id)] == expected_build_id
+        # assert strip_bytes[0x260 : 0x260 + len(expected_build_id)] == expected_build_id
+        # assert exe_bytes[0x260 : 0x260 + len(expected_build_id)] == expected_build_id
 
-        intel_hex = IntelHex(str(hex_filepath))
-        assert (
-            intel_hex.tobinstr(0x8160, size=len(expected_build_id)) == expected_build_id
-        )
+        # intel_hex = IntelHex(str(hex_filepath))
+        # assert (
+        #     intel_hex.tobinstr(0x8160, size=len(expected_build_id)) == expected_build_id
+        # )
 
-        # Check that nothing else was changed
-        assert elf_bytes == expected_elf_filepath.read_bytes()
-        assert hex_bytes == expected_hex_filepath.read_bytes()
-        assert bin_bytes == expected_bin_filepath.read_bytes()
-        assert strip_bytes == expected_strip_filepath.read_bytes()
-        assert exe_bytes == expected_exe_filepath.read_bytes()
+        # # Check that nothing else was changed
+        # assert elf_bytes == expected_elf_filepath.read_bytes()
+        # assert hex_bytes == expected_hex_filepath.read_bytes()
+        # assert bin_bytes == expected_bin_filepath.read_bytes()
+        # assert strip_bytes == expected_strip_filepath.read_bytes()
+        # assert exe_bytes == expected_exe_filepath.read_bytes()
