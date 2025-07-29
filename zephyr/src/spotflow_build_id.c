053b48b8bcc4ba99ed3c919bea6a028f83876071
roberthusak <robert@husak.cloud>
2025-07-29 22:43:28 +0200

    Remove build ID binary descriptor ID from Kconfig

--- zephyr/src/spotflow_build_id.c
+++ zephyr/src/spotflow_build_id.c
@@ -2,8 +2,10 @@
 
 #include "spotflow_build_id.h"
 
+#define SPOTFLOW_BINDESC_ID_BUILD_ID 0x5f0
+
 /* The actual build ID will be written to this array in a post-build command */
-BINDESC_BYTES_DEFINE(spotflow_build_id, CONFIG_SPOTFLOW_BINDESC_ID_BUILD_ID,
+BINDESC_BYTES_DEFINE(spotflow_build_id, SPOTFLOW_BINDESC_ID_BUILD_ID,
 		     ({ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }));
 
 void spotflow_build_id_get(const uint8_t** build_id, uint16_t* build_id_len)
