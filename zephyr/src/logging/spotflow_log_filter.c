8b1bb312143d79a7cdd17093d05e955627427739
roberthusak <robert@husak.cloud>
2025-10-02 12:39:44 +0200

    Fix formatting

--- zephyr/src/logging/spotflow_log_filter.c
+++ zephyr/src/logging/spotflow_log_filter.c
@@ -4,6 +4,6 @@
 
 bool spotflow_log_filter_allow_msg(struct log_msg* log_msg)
 {
-        uint8_t level = log_msg_get_level(log_msg);
-        return level <= CONFIG_SPOTFLOW_DEFAULT_SENT_LOG_LEVEL;
+	uint8_t level = log_msg_get_level(log_msg);
+	return level <= CONFIG_SPOTFLOW_DEFAULT_SENT_LOG_LEVEL;
 }
