019814a697128717e8adf6482fb15241fc7a8f93
Noor Haq <noorhaq55@gmail.com>
2025-10-07 19:47:28 +0500

    Removed unused functions in cbor log.

--- esp_idf/spotflow/src/logging/spotflow_log_cbor.c
+++ esp_idf/spotflow/src/logging/spotflow_log_cbor.c
@@ -15,163 +15,13 @@
 #define KEY_SEQUENCE_NUMBER 0x0D
 
 /**
- * @brief To verify CBOR validity
+ * @brief To create the message format for logs in CBOR format
  * 
+ * @param body 
+ * @param severity 
+ * @param out_len 
+ * @return uint8_t* 
  */
-#define CBOR_CHECK(a, str, goto_tag, ret_value, ...)                              \
-    do                                                                            \
-    {                                                                             \
-        if ((a) != CborNoError)                                                   \
-        {                                                                         \
-            SPOTFLOW_LOG("%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
-            ret = ret_value;                                                      \
-            goto goto_tag;                                                        \
-        }                                                                         \
-    } while (0)
-
-static void indent(int nestingLevel)
-{
-    while (nestingLevel--) {
-        SPOTFLOW_LOG("  ");
-    }
-}
-
-static void dumpbytes(const uint8_t *buf, size_t len)
-{
-    while (len--) {
-        SPOTFLOW_LOG("%02X ", *buf++);
-    }
-}
-
-/**
- * Decode CBOR data manually
- */
-static CborError example_dump_cbor_buffer(CborValue *it, int nestingLevel)
-{
-    CborError ret = CborNoError;
-    while (!cbor_value_at_end(it)) {
-        CborType type = cbor_value_get_type(it);
-
-        indent(nestingLevel);
-        switch (type) {
-        case CborArrayType: {
-            CborValue recursed;
-            assert(cbor_value_is_container(it));
-            puts("Array[");
-            ret = cbor_value_enter_container(it, &recursed);
-            CBOR_CHECK(ret, "enter container failed", err, ret);
-            ret = example_dump_cbor_buffer(&recursed, nestingLevel + 1);
-            CBOR_CHECK(ret, "recursive dump failed", err, ret);
-            ret = cbor_value_leave_container(it, &recursed);
-            CBOR_CHECK(ret, "leave container failed", err, ret);
-            indent(nestingLevel);
-            puts("]");
-            continue;
-        }
-        case CborMapType: {
-            CborValue recursed;
-            assert(cbor_value_is_container(it));
-            puts("Map{");
-            ret = cbor_value_enter_container(it, &recursed);
-            CBOR_CHECK(ret, "enter container failed", err, ret);
-            ret = example_dump_cbor_buffer(&recursed, nestingLevel + 1);
-            CBOR_CHECK(ret, "recursive dump failed", err, ret);
-            ret = cbor_value_leave_container(it, &recursed);
-            CBOR_CHECK(ret, "leave container failed", err, ret);
-            indent(nestingLevel);
-            puts("}");
-            continue;
-        }
-        case CborIntegerType: {
-            int64_t val;
-            ret = cbor_value_get_int64(it, &val);
-            CBOR_CHECK(ret, "parse int64 failed", err, ret);
-            SPOTFLOW_LOG("%lld\n", (long long)val);
-            break;
-        }
-        case CborByteStringType: {
-            uint8_t *buf;
-            size_t n;
-            ret = cbor_value_dup_byte_string(it, &buf, &n, it);
-            CBOR_CHECK(ret, "parse byte string failed", err, ret);
-            dumpbytes(buf, n);
-            puts("");
-            free(buf);
-            continue;
-        }
-        case CborTextStringType: {
-            char *buf;
-            size_t n;
-            ret = cbor_value_dup_text_string(it, &buf, &n, it);
-            CBOR_CHECK(ret, "parse text string failed", err, ret);
-            puts(buf);
-            free(buf);
-            continue;
-        }
-        case CborTagType: {
-            CborTag tag;
-            ret = cbor_value_get_tag(it, &tag);
-            CBOR_CHECK(ret, "parse tag failed", err, ret);
-            SPOTFLOW_LOG("Tag(%lld)\n", (long long)tag);
-            break;
-        }
-        case CborSimpleType: {
-            uint8_t type;
-            ret = cbor_value_get_simple_type(it, &type);
-            CBOR_CHECK(ret, "parse simple type failed", err, ret);
-            SPOTFLOW_LOG("simple(%u)\n", type);
-            break;
-        }
-        case CborNullType:
-            puts("null");
-            break;
-        case CborUndefinedType:
-            puts("undefined");
-            break;
-        case CborBooleanType: {
-            bool val;
-            ret = cbor_value_get_boolean(it, &val);
-            CBOR_CHECK(ret, "parse boolean type failed", err, ret);
-            puts(val ? "true" : "false");
-            break;
-        }
-        case CborHalfFloatType: {
-            uint16_t val;
-            ret = cbor_value_get_half_float(it, &val);
-            CBOR_CHECK(ret, "parse half float type failed", err, ret);
-            SPOTFLOW_LOG("__f16(%04x)\n", val);
-            break;
-        }
-        case CborFloatType: {
-            float val;
-            ret = cbor_value_get_float(it, &val);
-            CBOR_CHECK(ret, "parse float type failed", err, ret);
-            SPOTFLOW_LOG("%g\n", val);
-            break;
-        }
-        case CborDoubleType: {
-            double val;
-            ret = cbor_value_get_double(it, &val);
-            CBOR_CHECK(ret, "parse double float type failed", err, ret);
-            SPOTFLOW_LOG("%g\n", val);
-            break;
-        }
-        case CborInvalidType: {
-            ret = CborErrorUnknownType;
-            CBOR_CHECK(ret, "unknown cbor type", err, ret);
-            break;
-        }
-        }
-
-        ret = cbor_value_advance_fixed(it);
-        CBOR_CHECK(ret, "fix value failed", err, ret);
-    }
-    return CborNoError;
-err:
-    return ret;
-}
-
-
 uint8_t* log_cbor(char* body, uint8_t severity, size_t *out_len)
 {
     // Buffer to create array to cointain several items
@@ -225,11 +75,6 @@ uint8_t* log_cbor(char* body, uint8_t severity, size_t *out_len)
     
     *out_len = cbor_encoder_get_buffer_size(&array_encoder, buf);
     SPOTFLOW_LOG("encoded buffer size %d", *out_len);
-    #ifdef CONFIG_SPOTFLOW_DEBUG_MESSAGE_TERMINAL
-        CborParser root_parser;
-        CborValue it;
-        cbor_parser_init(buf, CONFIG_SPOTFLOW_CBOR_LOG_MAX_LEN, 0, &root_parser, &it);
-        example_dump_cbor_buffer(&it, 0);
-    #endif
+
     return buf;
 }
