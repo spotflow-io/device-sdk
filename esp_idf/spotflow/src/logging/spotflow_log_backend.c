b1024c7ab691a54deb0a82d2878ba159f6116442
Noor Haq <noorhaq55@gmail.com>
2025-10-07 20:22:25 +0500

    Changed from stack to heap in spotflow_log_backend.

--- esp_idf/spotflow/src/logging/spotflow_log_backend.c
+++ esp_idf/spotflow/src/logging/spotflow_log_backend.c
@@ -5,9 +5,22 @@ static const char *TAG = "spotflow_testing";
 
 int spotflow_log_backend(const char *fmt, va_list args)
 {
-    // Format the log string into a buffer
-    char buffer[CONFIG_SPOTFLOW_LOG_BUFFER_SIZE];
-    int len = vsnprintf(buffer, sizeof(buffer), fmt, args);
+    int len = vsnprintf(NULL, 0, fmt, args);  // Get the required length
+    
+    // If the len is smaller than 0 or if the log is bigger than the set buffer size.
+    if (len < 0 || len > CONFIG_SPOTFLOW_LOG_BUFFER_SIZE) {
+        SPOTFLOW_LOG("Spotflow Log buffer not enough. Increase size to incorporate long messages.");
+        return 0;
+    }
+
+    // Dynamically assign memory in heap depending on the log size. 
+    char *buffer = malloc(len + 1);  // +1 for null terminator
+    if (!buffer) {
+        SPOTFLOW_LOG("Error not able to assign memory.");
+        return 0;
+    }
+    len++; // Increasing the size of len to include the null terminator
+    len = vsnprintf(buffer, len, fmt, args);
 
 #if CONFIG_USE_JSON_PAYLOAD
     char *log_severity = NULL;
