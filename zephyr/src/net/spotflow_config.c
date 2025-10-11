3a0b5ee43c9b41be1b80be60a09c97486405381e
roberthusak <robert@husak.cloud>
2025-10-11 13:27:17 +0200

    Move configuration persistence to separate module

--- zephyr/src/net/spotflow_config.c
+++ zephyr/src/net/spotflow_config.c
@@ -2,27 +2,17 @@
 #include <stdint.h>
 
 #include <zephyr/logging/log.h>
-#include <zephyr/settings/settings.h>
 
 #include "logging/spotflow_log_backend.h"
 #include "logging/spotflow_log_cbor.h"
 #include "net/spotflow_config.h"
 #include "net/spotflow_config_cbor.h"
+#include "net/spotflow_config_persistence.h"
 #include "net/spotflow_mqtt.h"
 
 LOG_MODULE_DECLARE(spotflow_net, CONFIG_SPOTFLOW_MODULE_DEFAULT_LOG_LEVEL);
 
-#define SPOTFLOW_SETTINGS_PACKAGE "spotflow"
-#define SPOTFLOW_SETTINGS_KEY_SENT_LOG_LEVEL "sent_log_level"
-#define SPOTFLOW_SETTINGS_PATH_SENT_LOG_LEVEL \
-	SPOTFLOW_SETTINGS_PACKAGE "/" SPOTFLOW_SETTINGS_KEY_SENT_LOG_LEVEL
-
 static volatile uint8_t sent_log_level = CONFIG_SPOTFLOW_DEFAULT_SENT_LOG_LEVEL;
-
-#if CONFIG_SPOTFLOW_SETTINGS
-static int settings_direct_load_callback(const char* key, size_t len, settings_read_cb read_cb,
-					 void* cb_arg, void* param);
-#endif
 
 static void set_sent_log_level(uint8_t level);
 static void add_log_severity_to_reported_msg(struct spotflow_config_reported_msg* reported_msg);
@@ -35,56 +25,18 @@ uint8_t spotflow_config_get_sent_log_level()
 
 void spotflow_config_init()
 {
-	/* Ensure that the update to the runtime filter is applied */
-	set_sent_log_level(sent_log_level);
+	struct spotflow_config_persisted_settings persisted_settings;
 
-#if CONFIG_SPOTFLOW_SETTINGS
+	spotflow_config_persistence_try_init();
+	spotflow_config_persistence_try_load(&persisted_settings);
 
-	int ret = settings_subsys_init();
-	if (ret != 0) {
-		LOG_ERR("Failed to initialize settings subsystem, persisting configuration will "
-			"not work");
-		return;
-	}
+	uint8_t initial_sent_log_level = persisted_settings.contains_sent_log_level
+	    ? persisted_settings.sent_log_level
+	    : sent_log_level;
 
-	ret = settings_load_subtree_direct(SPOTFLOW_SETTINGS_PACKAGE, settings_direct_load_callback,
-					   NULL);
-	if (ret != 0) {
-		LOG_ERR("Failed to load persisted Spotflow configuration");
-		return;
-	}
-
-	LOG_INF("Persisted Spotflow configuration loaded");
-
-#endif
+	/* Ensure that the update to the runtime filter is applied if available */
+	set_sent_log_level(initial_sent_log_level);
 }
-
-#if CONFIG_SPOTFLOW_SETTINGS
-static int settings_direct_load_callback(const char* key, size_t len, settings_read_cb read_cb,
-					 void* cb_arg, void* param)
-{
-	if (strcmp(key, SPOTFLOW_SETTINGS_KEY_SENT_LOG_LEVEL) == 0) {
-		if (len != sizeof(sent_log_level)) {
-			LOG_ERR("Invalid length for sent log level setting");
-			return -EINVAL;
-		}
-
-		uint8_t persisted_sent_log_level;
-		int ret =
-		    read_cb(cb_arg, &persisted_sent_log_level, sizeof(persisted_sent_log_level));
-		if (ret < 0) {
-			LOG_ERR("Failed to read sent log level setting: %d", ret);
-			return ret;
-		}
-
-		LOG_DBG("Persisted sent log level loaded: %d", persisted_sent_log_level);
-
-		set_sent_log_level(persisted_sent_log_level);
-	}
-
-	return 0;
-}
-#endif
 
 int spotflow_config_init_session()
 {
@@ -155,15 +107,11 @@ static void handle_desired_msg(uint8_t* payload, size_t len)
 
 		set_sent_log_level(new_sent_log_level);
 
-#if CONFIG_SPOTFLOW_SETTINGS
-		rc = settings_save_one(SPOTFLOW_SETTINGS_PATH_SENT_LOG_LEVEL, &new_sent_log_level,
-				       sizeof(sent_log_level));
-		if (rc < 0) {
-			LOG_ERR("Failed to persist updated sent log level setting: %d", rc);
-		} else {
-			LOG_DBG("Updated sent log level setting persisted");
-		}
-#endif
+		struct spotflow_config_persisted_settings persisted_settings = {
+			.contains_sent_log_level = true,
+			.sent_log_level = new_sent_log_level,
+		};
+		spotflow_config_persistence_try_save(&persisted_settings);
 
 		add_log_severity_to_reported_msg(&reported_msg);
 	}
