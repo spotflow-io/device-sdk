d03b145929c99b322ac2d2558420f65c37da0d0e
Noor Haq <noorhaq55@gmail.com>
2025-10-13 20:38:27 +0500

    Added Unused config errors.

--- esp_idf/spotflow/spotflow.c
+++ esp_idf/spotflow/spotflow.c
@@ -9,6 +9,16 @@
 vprintf_like_t original_vprintf = NULL;
 
 /**
+ * @brief For Unused Configurations. 
+ * 
+ */
+void Spotflow_Todo(void)
+{
+    #ifdef SPOTFLOW_GENERATE_BUILD_ID
+    SPOTFLOW_LOG("Not implemented yet.\n");
+    #endif
+}
+/**
  * @brief 
  * @details To utilize the esp_log_set_vprintf function to expose the logs
  */
@@ -16,6 +26,7 @@ void spotflow_init(void)
 {
     original_vprintf = esp_log_set_vprintf(spotflow_log_backend);
 
+    Spotflow_Todo(); //Checking for unused set Configs.
     mqtt_app_start(); // Calling the mqtt_start from the init function.
 
     queue_init(); //Initilize the queue
