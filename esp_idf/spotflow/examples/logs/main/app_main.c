81c87a52aff74840e7bdc3f8453aca4e95c4f047
Noor Haq <noorhaq55@gmail.com>
2025-10-13 20:34:11 +0500

    Removed unused imports from example.

--- esp_idf/spotflow/examples/logs/main/app_main.c
+++ esp_idf/spotflow/examples/logs/main/app_main.c
@@ -3,16 +3,11 @@
 #include <stddef.h>
 #include <string.h>
 #include "esp_system.h"
-#include "esp_partition.h"
-#include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "protocol_examples_common.h"
 
 #include "esp_log.h"
-#include "mqtt_client.h"
-#include "esp_tls.h"
-#include "esp_ota_ops.h"
 #include "spotflow.h"
 #include <sys/param.h>
 
