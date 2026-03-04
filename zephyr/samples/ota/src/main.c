cdb8dfa29d7eb9d052c0236381f6856907ced69b
roberthusak <robert@husak.cloud>
2026-03-04 12:01:42 +0100

    Add image confirmation to OTA sample

--- zephyr/samples/ota/src/main.c
+++ zephyr/samples/ota/src/main.c
@@ -1,4 +1,5 @@
 #include <zephyr/logging/log.h>
+#include <zephyr/dfu/mcuboot.h>
 
 #include "net.h"
 
@@ -6,7 +7,24 @@ LOG_MODULE_REGISTER(ota_sample, LOG_LEVEL_INF);
 
 int main(void)
 {
+	/* TODO: Add a Kconfig option and instructions to README */
+	/* Uncomment for versions to be downloaded */
+	// LOG_INF("I was downloaded from the web!");
+
 	LOG_INF("Starting Spotflow OTA example");
+
+	if (boot_is_img_confirmed()) {
+		LOG_INF("Current image is confirmed");
+	} else {
+		LOG_INF("Current image is NOT confirmed (test mode)");
+		LOG_INF("Confirming current image...");
+		int ret = boot_write_img_confirmed();
+		if (ret) {
+			LOG_ERR("ERROR: Failed to confirm image: %d", ret);
+		} else {
+			LOG_INF("Image confirmed successfully");
+		}
+	}
 
 	/* Wait for the initialization of network device */
 	k_sleep(K_SECONDS(1));
