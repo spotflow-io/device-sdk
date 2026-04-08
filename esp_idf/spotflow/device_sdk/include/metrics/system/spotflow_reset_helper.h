0eeb04ee11cb0c750b5911021e16002f992c8d5e
Noor Haq <noorhaq55@gmail.com>
2026-04-08 16:12:37 +0000

    Initial commit. File changes.

---
+++ esp_idf/spotflow/device_sdk/include/metrics/system/spotflow_reset_helper.h
@@ -0,0 +1,16 @@
+#ifndef METRICS_SPOTFLOW_RESET_HELPER_H
+#define METRICS_SPOTFLOW_RESET_HELPER_H
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void spotflow_report_reboot_reason(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //METRICS_SPOTFLOW_RESET_HELPER_H
