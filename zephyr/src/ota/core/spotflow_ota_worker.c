5192970e8d47b89be3e0341411047f2f7d1353bc
roberthusak <robert@husak.cloud>
2026-08-10 15:49:49 +0200

    Increase maximum write attempt delay to 5 minutes

--- zephyr/src/ota/core/spotflow_ota_worker.c
+++ zephyr/src/ota/core/spotflow_ota_worker.c
@@ -23,7 +23,8 @@ LOG_MODULE_DECLARE(spotflow_ota, CONFIG_SPOTFLOW_OTA_LOG_LEVEL);
 #endif /* CONFIG_SPOTFLOW_OTA_THREAD_STACK_SIZE */
 
 #define OTA_WORKER_RETRY_INITIAL_DELAY_MS 20U
-#define OTA_WORKER_RETRY_MAX_SHIFT 6U
+#define OTA_WORKER_RETRY_MAX_DELAY_MS (5U * 60U * 1000U)
+#define OTA_WORKER_RETRY_MAX_SHIFT 14U
 
 enum worker_operation_type {
 	WORKER_OPERATION_NONE,
@@ -670,7 +671,8 @@ static void advance_report_operation(enum report_operation_stage stage)
 static void schedule_worker_retry(void)
 {
 	uint8_t shift = MIN(operation.retry_count, OTA_WORKER_RETRY_MAX_SHIFT);
-	uint32_t delay_ms = OTA_WORKER_RETRY_INITIAL_DELAY_MS << shift;
+	uint32_t delay_ms =
+		MIN(OTA_WORKER_RETRY_INITIAL_DELAY_MS << shift, OTA_WORKER_RETRY_MAX_DELAY_MS);
 
 	if (operation.retry_count < UINT8_MAX) {
 		operation.retry_count++;
